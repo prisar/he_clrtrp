77efd2f672dd30dcf67167f3b8908c0f3d31f126
prisar <pritimaysarkar2010@gmail.com>
2018-01-28 19:08:03 +0530

    handle response

--- src/Github.js
+++ src/Github.js
@@ -6,21 +6,35 @@ const urlForUsername = username =>
 class GitHub extends Component {
     constructor(props){
         super(props)
-        this.state = {}
+        this.state = {
+            requestFailed: false
+        }
     }
 
     componentDidMount(){
         fetch(urlForUsername(this.props.username))
+            .then(response => {
+                if (!response.ok) {
+                    throw Error("Network request failed")
+                }
+
+                return response
+            })
             .then(d => d.json())
             .then(d => {
                 this.setState({
                     githubData: d
+                })
+            }, () => {
+                this.setState({
+                    requestFailed: true
                 })
             })
     }
 
     render(){
 
+        if(this.state.requestFailed) return <p>Failed!</p>
         if(!this.state.githubData) return <p>Loading...</p>
         return (
             <div>
